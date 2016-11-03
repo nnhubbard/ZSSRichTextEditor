7175aaae0f0afc38f634e592635b511289d6cd58
Yedidya Kennard <yedidyak@wix.com>
2016-11-03 17:46:44 +0200

    Fixed possible scroll bug
    Removed 'var' that took new scroll position out of scope

--- ZSSRichTextEditor/Source/ZSSRichTextEditor.js
+++ ZSSRichTextEditor/Source/ZSSRichTextEditor.js
@@ -161,7 +161,7 @@ zss_editor.calculateEditorHeightWithCaretPosition = function() {
     if (c < offsetY) {
         newPos = c;
     } else if (c > (offsetY + height - padding)) {
-        var newPos = c - height + padding - 18;
+        newPos = c - height + padding - 18;
     }
     
     window.scrollTo(0, newPos);
